5b1c74af008b9996b1be36d4b6b160175a8a8d69
Nicolas AMATI <skkyz3r.dev@gmail.com>
2023-11-01 23:30:02 +0100

    feat: add CategoryFormType

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryFormType;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,6 +20,27 @@ class CategoryController extends AbstractController
 
         return $this->render("category/index.html.twig", [
             "categories" => $categories,
+        ]);
+    }
+
+    #[Route("/category/edit/{id}", name: "category_edit", requirements: ["id" => "\d+"], methods: ["GET", "POST"])]
+    public function edit(Category $category, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            $this->addFlash("success", "Catégorie modifiée avec succès !");
+
+            return $this->redirectToRoute("app_category");
+        }
+
+        return $this->render("category/edit.html.twig", [
+            "form" => $form,
+            "category" => $category,
         ]);
     }
 }
