c7e00b22f9a6ed5cdc9c2f300e0ff4266077112f
Nicolas AMATI <skkyz3r.dev@gmail.com>
2023-11-02 12:05:21 +0100

    fix: Fix form types errors

--- src/Form/FilmFormType.php
+++ src/Form/FilmFormType.php
@@ -8,6 +8,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,10 +19,14 @@ class FilmFormType extends AbstractType
         $builder
             ->add('title', TextType::class)
             ->add('summary', TextareaType::class)
+            ->add("author", TextType::class)
             ->add('category', EntityType::class, [
                 "class" => Category::class,
                 "multiple" => false,
                 "choice_label" => "name"
+            ])
+            ->add("created_at", DateTimeType::class, [
+                "input" => "datetime_immutable"
             ])
         ;
     }
